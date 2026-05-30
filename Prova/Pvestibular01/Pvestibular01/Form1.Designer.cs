7f1f6773f9b88c725ff5bd8ac25fea351afc5669
nicollascalixto <nicocalixto7@gmail.com>
2026-05-29 22:20:33 -0300

    Add files via upload

---
+++ Prova/Pvestibular01/Pvestibular01/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Pvestibular01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReceberDados = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.ltsbxResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnReceberDados
+            // 
+            this.btnReceberDados.Location = new System.Drawing.Point(377, 132);
+            this.btnReceberDados.Name = "btnReceberDados";
+            this.btnReceberDados.Size = new System.Drawing.Size(216, 87);
+            this.btnReceberDados.TabIndex = 0;
+            this.btnReceberDados.Text = "Receber Dados";
+            this.btnReceberDados.UseVisualStyleBackColor = true;
+            this.btnReceberDados.Click += new System.EventHandler(this.btnReceberDados_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(377, 278);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(216, 84);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // ltsbxResultado
+            // 
+            this.ltsbxResultado.FormattingEnabled = true;
+            this.ltsbxResultado.ItemHeight = 20;
+            this.ltsbxResultado.Location = new System.Drawing.Point(657, 16);
+            this.ltsbxResultado.Name = "ltsbxResultado";
+            this.ltsbxResultado.Size = new System.Drawing.Size(433, 524);
+            this.ltsbxResultado.TabIndex = 2;
+            this.ltsbxResultado.SelectedIndexChanged += new System.EventHandler(this.ltsbxResultado_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 618);
+            this.Controls.Add(this.ltsbxResultado);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnReceberDados);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReceberDados;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.ListBox ltsbxResultado;
+    }
+}
+
---
+++ Prova/Pvestibular01/Pvestibular01/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+using Microsoft.VisualBasic;
+namespace Pvestibular01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReceberDados_Click(object sender, EventArgs e)
+        {
+            int[,] Dados = new int[3, 5];
+            string aux = "";
+            int[] totalCurso = new int[3];
+
+            for (int curso = 0; curso < 3; curso ++) {
+                for ( int ano = 0; ano < 5; ano ++)
+                {
+                            aux = Interaction.InputBox("Total do Curso:  " + (curso +1) + " do ano " + (ano +1)  + ":");
+
+                        if (aux == "")
+                    {
+                        return;
+                    }
+                        if (!int.TryParse(aux, out Dados[curso,ano]) || Dados[curso,ano] < 0)
+                    {
+                        MessageBox.Show("valor Invalido");
+                        ano--;
+                    }
+                        else
+                    {
+                         totalCurso[curso] += Dados[curso, ano];
+                    }
+
+                }
+                        int TotalGeral = 0;
+                for (int curso1 = 0; curso1 < 3; curso1++)
+                {
+                    ltsbxResultado.Items.Add($"Total do Curso:  {totalCurso[curso1]} ");
+                    TotalGeral += totalCurso[curso1];
+                }
+                ltsbxResultado.Items.Add("______________________________");
+                ltsbxResultado.Items.Add($"Total Geral: {TotalGeral}");
+            }
+            
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+                    ltsbxResultado.Items.Clear();
+        }
+
+        private void ltsbxResultado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
